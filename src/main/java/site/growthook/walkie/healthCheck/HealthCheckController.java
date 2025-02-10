b127eeafd03858e5214104f00dc50e08a8af20d7
김수영 <kswim8914@gmail.com>
2025-02-11 00:14:35 +0900

    init: 헬스체크 API 생성 및 테스트

---
+++ src/main/java/site/growthook/walkie/healthCheck/HealthCheckController.java
@@ -0,0 +1,13 @@
+package site.growthook.walkie.healthCheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("api/v1/healthCheck")
+    public String healthCheck() {
+        return "OK";
+    }
+}
