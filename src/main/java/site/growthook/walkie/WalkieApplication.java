ac4b1b889b522d3ce7cf1e8a925817de8a55321a
김수영 <kswim8914@gmail.com>
2025-02-11 00:14:21 +0900

    init: 프로젝트 생성

---
+++ src/main/java/site/growthook/walkie/WalkieApplication.java
@@ -0,0 +1,16 @@
+package site.growthook.walkie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class WalkieApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HealthCheck started");
+		SpringApplication.run(WalkieApplication.class, args);
+		System.out.println("HealthCheck finished");
+	}
+
+}
---
+++ src/test/java/site/growthook/walkie/WalkieApplicationTests.java
@@ -0,0 +1,13 @@
+package site.growthook.walkie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
