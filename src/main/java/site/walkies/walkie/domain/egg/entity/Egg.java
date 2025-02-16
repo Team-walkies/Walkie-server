e43c95b3005d9b1f89b3cf21b8a5b0383433bc3e
jaejunlee11 <sktks11@naver.com>
2025-02-17 00:24:00 +0900

    feat: #10-Egg entity 생성

---
+++ src/main/java/site/walkies/walkie/domain/egg/entity/Egg.java
@@ -0,0 +1,41 @@
+package site.walkies.walkie.domain.egg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "egg")
+@Getter
+public class Egg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "rank")
+    private Integer rank;
+
+    @Column(name = "need_step")
+    private Integer needStep;
+
+    @Column(name = "now_step")
+    private Integer nowStep;
+
+    @Column(name = "obtained_position")
+    private String obtainedPosition;
+
+    @Column(name = "obtained_date")
+    private LocalDate obtainedDate;
+
+    @Column(name = "picked")
+    private Boolean picked;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private Member member;
+}
