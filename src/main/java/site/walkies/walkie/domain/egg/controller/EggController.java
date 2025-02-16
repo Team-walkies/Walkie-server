b7eea93800779a608e8e9ab5ab70527329ebfa1d
jaejunlee11 <sktks11@naver.com>
2025-02-16 22:10:05 +0900

    feat: #6-Egg controller 생성

---
+++ src/main/java/site/walkies/walkie/domain/egg/controller/EggController.java
@@ -0,0 +1,10 @@
+package site.walkies.walkie.domain.egg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eggs")
+public class EggController {
+
+}
