01375a36773f677cb5d6ce7db91562173eb4680f
jaejunlee11 <sktks11@naver.com>
2025-02-16 22:10:18 +0900

    feat: #6-Egg entity 생성

---
+++ src/main/java/site/walkies/walkie/domain/egg/entity/Egg.java
@@ -0,0 +1,13 @@
+package site.walkies.walkie.domain.egg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "egg")
+@Getter
+@Setter
+public class Egg {
+}
