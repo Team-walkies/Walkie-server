87aa42ca6d2aad6c35dd7630c868764271c15e65
jaejunlee11 <sktks11@naver.com>
2025-02-21 00:19:56 +0900

    feat : egg repository 생성

--- src/main/java/site/walkies/walkie/domain/egg/repository/EggRepository.java
+++ src/main/java/site/walkies/walkie/domain/egg/repository/EggRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.walkies.walkie.domain.egg.entity.Egg;
 
+import java.util.List;
+
 @Repository
 public interface EggRepository extends JpaRepository<Egg, Long> {
+    List<Egg> findAllByUserId(Long userId);
 }
