f96b03341e889768e5e370f1ddbf86532741bfec
jaejunlee11 <sktks11@naver.com>
2025-02-21 23:52:15 +0900

    feat : CharacterBorn 생성 서비스 개발

---
+++ src/main/java/site/walkies/walkie/domain/character/repository/UserCharacterBornRepository.java
@@ -0,0 +1,9 @@
+package site.walkies.walkie.domain.character.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.walkies.walkie.domain.character.entity.UserCharacterBorn;
+
+@Repository
+public interface UserCharacterBornRepository extends JpaRepository<UserCharacterBorn, Long> {
+}
---
+++ src/main/java/site/walkies/walkie/domain/character/service/CharacterService.java
@@ -0,0 +1,47 @@
+package site.walkies.walkie.domain.character.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.walkies.walkie.domain.character.entity.UserCharacterBorn;
+import site.walkies.walkie.domain.character.repository.UserCharacterBornRepository;
+import site.walkies.walkie.domain.character.repository.UserCharacterRepository;
+import site.walkies.walkie.domain.member.entity.Member;
+import site.walkies.walkie.domain.character.entity.UserCharacter;
+import site.walkies.walkie.domain.member.repository.MemberRepository;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class CharacterService {
+    private final UserCharacterRepository userCharacterRepository;
+    private final UserCharacterBornRepository userCharacterBornRepository;
+    private final MemberRepository memberRepository;
+
+    // 캐릭터 생성 함수
+    // input : userId, rank(캐릭터 등급), type(캐릭터 타입), characterClass(캐릭터 소분류), picked(같이 다니는 여부)
+    // output : character
+    public UserCharacter createCharacter(Long userId, int rank, int type, int characterClass, boolean picked) {
+        Member member = memberRepository.findById(userId).orElse(null);
+        if (member == null) { return null; }
+
+        UserCharacter userCharacter = new UserCharacter(rank, type, characterClass,picked, member);
+        userCharacterRepository.save(userCharacter);
+
+        return userCharacter;
+    }
+
+    // 캐릭터 부화 함수
+    // input : characterId, obtainDate(부화 날짜), obtainPosition(부화 위치)
+    // output : CharacterBorn
+    public UserCharacterBorn createCharacterBorn(Long characterId, LocalDate obtainedDate, String obtainedPosition) {
+        UserCharacter userCharacter = userCharacterRepository.findById(characterId).orElse(null);
+        if(userCharacter == null) { return null;}
+
+        UserCharacterBorn userCharacterBorn = new UserCharacterBorn(obtainedDate, obtainedPosition, userCharacter);
+        userCharacterBornRepository.save(userCharacterBorn);
+
+        return userCharacterBorn;
+    }
+
+}
