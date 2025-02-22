edab1da91e4060b2f554af46224fdcf5c72437c6
leejaejun <sktks11@naver.com>
2025-02-22 16:30:22 +0900

    feat : egg 생성 서비스 구현

--- src/main/java/site/walkies/walkie/domain/egg/service/EggService.java
+++ src/main/java/site/walkies/walkie/domain/egg/service/EggService.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 import site.walkies.walkie.domain.egg.entity.Egg;
 import site.walkies.walkie.domain.egg.repository.EggRepository;
 import site.walkies.walkie.domain.egg.service.dto.response.GetEggResponse;
+import site.walkies.walkie.domain.member.entity.Member;
+import site.walkies.walkie.domain.member.repository.MemberRepository;
+import site.walkies.walkie.global.enums.EggsProbability;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +18,7 @@ import java.util.List;
 public class EggService {
 
     private final EggRepository repository;
+    private final MemberRepository memberRepository;
 
     // 보유한 알 리스트 조회 method
     // input : user ID
@@ -27,5 +32,28 @@ public class EggService {
         }
 
         return eggs;
+    }
+
+    // egg 생성 method
+    // input : userId, obtainedPosition(얻은 위치), obtainedDate(얻은 날짜)
+    // output : Egg
+    public Egg createEgg (long userId, String obtainedPosition, LocalDate obtainedDate) {
+        Member member = memberRepository.findById(userId).orElse(null);
+        if (member == null) { return null; }
+
+        // 확률에 따른 알 생성
+        // 1.랜덤값 생성
+        double random = Math.random() * 100;
+        // 2. 범위에 따라 다른 Egg 생성
+        if(random <= EggsProbability.NORMAL_EGG.getProbability()) {
+            // 3. 확률에 따라 캐릭터 생성
+
+            
+            Egg egg = new Egg(EggsProbability.NORMAL_EGG.getRank(), 2000,0, obtainedPosition, obtainedDate, false,null,member);
+            return egg;
+        }
+
+
+        return null;
     }
 }
