6bf046c645e22f45a560eb83411780c48f28b4b5
김수영 <kswim8914@gmail.com>
2025-02-24 10:28:10 +0900

    feat: 컨트롤러 단에서 발생하는 예외를 중앙에서 처리하는 ExceptionHandler 추가

---
+++ src/main/java/site/walkies/walkie/global/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package site.walkies.walkie.global.web.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import site.walkies.walkie.global.web.dto.response.ExceptionResponse;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // @RestControllerAdvice와 @ExceptionHandler를 활용하여 컨트롤러 단에서 발생하는 예외를 중앙에서 처리
+
+    // CustomException에 대한 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponse> handleCustomException(CustomException ex){
+        log.error("CustomException 발생: ", ex);
+        ExceptionResponse response = ExceptionResponse.builder()
+                .status(ex.getErrorcode().getHttpStatus().value())
+                .message(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(response, ex.getErrorcode().getHttpStatus());
+    }
+
+    // 그 외의 모든 Exception에 대한 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleGeneralException(Exception ex){
+        log.error("Exception 발생: ", ex);
+        ExceptionResponse response = ExceptionResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("서버 내부 오류가 발생했습니다.")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
