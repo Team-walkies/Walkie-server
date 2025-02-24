e2ef1c6db1c0abd64878424798ba9024db2386c9
jaejunlee11 <sktks11@naver.com>
2025-02-25 01:50:11 +0900

    feat: 알 생성 테스트용 API 구현

--- src/main/java/site/walkies/walkie/domain/egg/controller/EggController.java
+++ src/main/java/site/walkies/walkie/domain/egg/controller/EggController.java
@@ -2,11 +2,15 @@ package site.walkies.walkie.domain.egg.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.walkies.walkie.domain.egg.entity.Egg;
 import site.walkies.walkie.domain.egg.service.EggService;
 import site.walkies.walkie.domain.egg.service.dto.response.GetEggResponse;
+import site.walkies.walkie.global.web.dto.response.SuccessResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,13 @@ public class EggController {
     public List<GetEggResponse> getAll() {
         List<GetEggResponse> responses = eggService.getEggsList(1);
         return responses;
+    }
+
+    // 알 생성 테스트 API
+    @PostMapping
+    public SuccessResponse<?> createEgg() {
+        Egg egg = eggService.createEgg(1, "테스트 장소", LocalDate.now());
+        System.out.println(egg);
+        return SuccessResponse.created(egg);
     }
 }
