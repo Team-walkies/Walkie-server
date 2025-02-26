611ddadd432d9803ddf60725db84ac4cfd970a5b
jaejunlee11 <sktks11@naver.com>
2025-02-27 00:47:20 +0900

    feat : 알 디테일 조회 API response 생성

--- src/main/java/site/walkies/walkie/domain/egg/controller/EggController.java
+++ src/main/java/site/walkies/walkie/domain/egg/controller/EggController.java
@@ -1,12 +1,10 @@
 package site.walkies.walkie.domain.egg.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.walkies.walkie.domain.egg.entity.Egg;
 import site.walkies.walkie.domain.egg.service.EggService;
+import site.walkies.walkie.domain.egg.service.dto.response.GetEggDetailResponse;
 import site.walkies.walkie.domain.egg.service.dto.response.GetEggResponse;
 import site.walkies.walkie.domain.egg.service.dto.response.PostEggResponse;
 import site.walkies.walkie.global.web.dto.response.SuccessResponse;
@@ -26,5 +24,12 @@ public class EggController {
     public SuccessResponse<List<GetEggResponse>> getAll() {
         List<GetEggResponse> responses = eggService.getEggsList(1);
         return SuccessResponse.ok(responses);
+    }
+
+    // 알 디테일 조회 API
+    @GetMapping("/{eggId}")
+    public SuccessResponse<GetEggDetailResponse> getEggDetail(@PathVariable Long eggId) {
+        GetEggDetailResponse response = eggService.getEggDetail(eggId);
+        return SuccessResponse.ok(response);
     }
 }
