e580bbb65b874f48d9dd97b7a042a0c8e02b8401
jaejunlee11 <sktks11@naver.com>
2025-03-02 01:37:31 +0900

    feat: 캐릭터 조회 컨트롤러 생성 및 개발

---
+++ src/main/java/site/walkies/walkie/domain/character/controller/UserCharacterController.java
@@ -0,0 +1,29 @@
+package site.walkies.walkie.domain.character.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import site.walkies.walkie.domain.character.service.CharacterService;
+import site.walkies.walkie.domain.character.service.dto.response.CharacterListResponse;
+import site.walkies.walkie.domain.character.service.dto.response.GetCharacterResponse;
+import site.walkies.walkie.global.web.dto.response.SuccessResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/characters")
+@RequiredArgsConstructor
+public class UserCharacterController {
+    private final CharacterService characterService;
+
+    @GetMapping
+    public SuccessResponse<CharacterListResponse> getCharacters(@RequestParam(required = false) Integer type) {
+        List<GetCharacterResponse> responses = characterService.getCharacters(2,type);
+
+        CharacterListResponse response = new CharacterListResponse(responses);
+
+        return SuccessResponse.ok(response);
+    }
+}
