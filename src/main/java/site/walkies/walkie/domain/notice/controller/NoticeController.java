d98e11933ed350938108a15f6052d1c17fa695e4
jaejunlee11 <sktks11@naver.com>
2025-03-03 22:03:55 +0900

    feat : 공지사항 리스트 조회 서비스 구현

--- src/main/java/site/walkies/walkie/domain/notice/controller/NoticeController.java
+++ src/main/java/site/walkies/walkie/domain/notice/controller/NoticeController.java
@@ -1,13 +1,11 @@
 package site.walkies.walkie.domain.notice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.walkies.walkie.domain.notice.service.NoticeService;
 import site.walkies.walkie.domain.notice.service.dto.request.PostNoticeRequest;
-import site.walkies.walkie.domain.notice.service.dto.response.PostNoticeResponse;
+import site.walkies.walkie.domain.notice.service.dto.response.GetListNoticeResponse;
+import site.walkies.walkie.domain.notice.service.dto.response.NoticeResponse;
 import site.walkies.walkie.global.web.dto.response.SuccessResponse;
 
 @RestController
@@ -17,8 +15,14 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping
-    public SuccessResponse<PostNoticeResponse> postNotice(@RequestBody PostNoticeRequest request) {
-        PostNoticeResponse response = noticeService.postNotice(request.getDate(),request.getTitle(),request.getDetail());
+    public SuccessResponse<NoticeResponse> postNotice(@RequestBody PostNoticeRequest request) {
+        NoticeResponse response = noticeService.postNotice(request.getDate(),request.getTitle(),request.getDetail());
         return SuccessResponse.created(response);
+    }
+
+    @GetMapping
+    public SuccessResponse<GetListNoticeResponse> getListNotice() {
+        GetListNoticeResponse response = noticeService.getListNotice();
+        return SuccessResponse.ok(response);
     }
 }
