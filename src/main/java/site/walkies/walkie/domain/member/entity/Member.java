41fcca0a79b30f1ce1745488598e467900914824
김수영 <kswim8914@gmail.com>
2025-03-07 18:25:32 +0900

    feat: kakao 로그인 기능 구현

--- src/main/java/site/walkies/walkie/domain/member/entity/Member.java
+++ src/main/java/site/walkies/walkie/domain/member/entity/Member.java
@@ -1,13 +1,16 @@
 package site.walkies.walkie.domain.member.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import site.walkies.walkie.domain.character.entity.UserCharacter;
 import site.walkies.walkie.domain.egg.entity.Egg;
 
 @Entity
 @Table(name = "member")
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -15,23 +18,26 @@ public class Member {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "providerId", nullable = false, unique = true)
+    private String providerId;
+
+    @Column(name = "provider", nullable = false)
+    private String provider;
+
     @Column(name = "nickname")
     private String nickname;
 
-    @Column(name = "password")
-    private String password;
+    @Column(name = "explored_spot")
+    private Integer exploredSpot;
 
-//    @Column(name = "explored_spot")
-//    private Integer exploredSpot;
-//
-//    @Column(name = "recorded_spot")
-//    private Integer recordedSpot;
-//
-//    @Column(name = "is_public")
-//    private Boolean isPublic;
-//
-//    @Column(name = "member_tier")
-//    private String memberTier;
+    @Column(name = "recorded_spot")
+    private Integer recordedSpot;
+
+    @Column(name = "is_public")
+    private Boolean isPublic;
+
+    @Column(name = "member_tier")
+    private String memberTier;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "leveling_egg_id")
@@ -40,4 +46,18 @@ public class Member {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "leveling_character_id")
     private UserCharacter levelingUserCharacter;
+
+    @Builder
+    public Member(Long id, String providerId, String provider, String nickname, Integer exploredSpot, Integer recordedSpot, Boolean isPublic, String memberTier, Egg levelingEgg, UserCharacter levelingUserCharacter) {
+        this.id = id;
+        this.providerId = providerId;
+        this.provider = provider;
+        this.nickname = nickname;
+        this.exploredSpot = exploredSpot;
+        this.recordedSpot = recordedSpot;
+        this.isPublic = isPublic;
+        this.memberTier = memberTier;
+        this.levelingEgg = levelingEgg;
+        this.levelingUserCharacter = levelingUserCharacter;
+    }
 }
