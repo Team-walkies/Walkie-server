446b2e430df4bd793627f01696d8afbe4d63a827
김수영 <kswim8914@gmail.com>
2025-03-07 18:22:17 +0900

    feat: 멤버 응답 DTO 생성

---
+++ src/main/java/site/walkies/walkie/domain/member/service/dto/response/MemberResponseDto.java
@@ -0,0 +1,34 @@
+package site.walkies.walkie.domain.member.service.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MemberResponseDto {
+    private Long id;
+    private String providerId;
+    private String provider;
+    private String nickname;
+    private Integer exploredSpot;
+    private Integer recordedSpot;
+    private Boolean isPublic;
+    private String memberTier;
+    private Long eggId;
+    private Long userCharacterId;
+
+    @Builder
+    public MemberResponseDto(Long id, String providerId, String provider, String nickname, Integer exploredSpot, Integer recordedSpot, Boolean isPublic, String memberTier, Long eggId, Long userCharacterId) {
+        this.id = id;
+        this.providerId = providerId;
+        this.provider = provider;
+        this.nickname = nickname;
+        this.exploredSpot = exploredSpot;
+        this.recordedSpot = recordedSpot;
+        this.isPublic = isPublic;
+        this.memberTier = memberTier;
+        this.eggId = eggId;
+        this.userCharacterId = userCharacterId;
+    }
+}
