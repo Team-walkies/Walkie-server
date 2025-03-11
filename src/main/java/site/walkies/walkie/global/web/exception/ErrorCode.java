dd62f735adadaf6a9498394ef6848ed44f734e02
김수영 <kswim8914@gmail.com>
2025-03-11 13:25:56 +0900

    feat: jwt 토큰 관련 에러 추가

--- src/main/java/site/walkies/walkie/global/web/exception/ErrorCode.java
+++ src/main/java/site/walkies/walkie/global/web/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
         USER_NOT_FOUND("존재하지 않는 유저입니다.", HttpStatus.UNAUTHORIZED),
     //    USER_VALIDATION_ERROR("유저 검증 오류가 발생했습니다.", HttpStatus.UNAUTHORIZED),
     //    INVALID_PASSWORD("올바르지 않은 비밀번호입니다.", HttpStatus.UNAUTHORIZED),
-    //    INVALID_ACCESS_TOKEN("올바르지 않은 ACCESSTOKEN 입니다", HttpStatus.UNAUTHORIZED),
+        INVALID_ACCESS_TOKEN("올바르지 않은 ACCESSTOKEN 입니다", HttpStatus.UNAUTHORIZED),
     //    JSON_PROCESSING_ERROR("JSON 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
     // 알 관련 예외
