ab1ed938993d819640a1762357d8d74e15b205a4
jaejunlee11 <sktks11@naver.com>
2025-03-22 00:46:00 +0900

    fix: 리뷰 리스트에 캐릭터 상세 정보 및 스팟 이름 추가

--- src/main/java/site/walkies/walkie/domain/review/service/dto/response/GetReviewResponse.java
+++ src/main/java/site/walkies/walkie/domain/review/service/dto/response/GetReviewResponse.java
@@ -10,13 +10,23 @@ import java.time.LocalTime;
 @Builder
 public class GetReviewResponse {
     private Long reviewId;
+
     private Long spotId;
+    // 스팟 이름 추가
+    private String spotName;
+
     private Double distance;
     private Integer step;
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime endTime;
+
     private Long characterId;
+    // 캐릭터 상세 정보 추가
+    private Integer rank;
+    private Integer type;
+    private Integer characterClass;
+
     private String pic;
     private Boolean reviewCd;
     private String review;
