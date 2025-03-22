0da6964f58494b44b1c504512071b18fe4ac4a19
김수영 <kswim8914@gmail.com>
2025-03-22 17:45:43 +0900

    feat: 같이 걷는 캐릭터 수정 요청을 받는 메서드 생성

--- src/main/java/site/walkies/walkie/domain/member/controller/MemberController.java
+++ src/main/java/site/walkies/walkie/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import site.walkies.walkie.domain.member.service.MemberService;
+import site.walkies.walkie.domain.member.service.dto.request.MemberUpdateCharacterRequestDto;
 import site.walkies.walkie.domain.member.service.dto.request.MemberUpdateRequestDto;
 import site.walkies.walkie.domain.member.service.dto.response.MemberResponseDto;
 import site.walkies.walkie.global.auth.dto.MemberPrincipal;
@@ -27,6 +28,14 @@ public class MemberController {
     @PatchMapping
     public SuccessResponse<MemberResponseDto> updateMemberInfo(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestBody MemberUpdateRequestDto memberUpdateRequestDto ) {
         MemberResponseDto memberResponseDto = memberService.updateMemberInfo(memberPrincipal.getMemberId(), memberUpdateRequestDto);
+        return SuccessResponse.updated(memberResponseDto);
+    }
+
+
+    // 함께 걷는 캐릭터 변경
+    @PatchMapping("/characters/play")
+    public SuccessResponse<MemberResponseDto> updateMemberLevelingCharacter(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestBody MemberUpdateCharacterRequestDto memberUpdateCharacterRequestDto ) {
+        MemberResponseDto memberResponseDto = memberService.updateMemberLevelingCharacter(memberPrincipal.getMemberId(), memberUpdateCharacterRequestDto);
         return SuccessResponse.updated(memberResponseDto);
     }
 }
