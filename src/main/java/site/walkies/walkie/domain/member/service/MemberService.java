6e149928c8f51d74c31ba15c400fce5e2983fa4f
김수영 <kswim8914@gmail.com>
2025-03-22 17:45:56 +0900

    feat: 같이 걷는 캐릭터 수정을 하는 메서드 생성

--- src/main/java/site/walkies/walkie/domain/member/service/MemberService.java
+++ src/main/java/site/walkies/walkie/domain/member/service/MemberService.java
@@ -3,8 +3,11 @@ package site.walkies.walkie.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.walkies.walkie.domain.character.entity.UserCharacter;
+import site.walkies.walkie.domain.character.repository.UserCharacterRepository;
 import site.walkies.walkie.domain.member.entity.Member;
 import site.walkies.walkie.domain.member.repository.MemberRepository;
+import site.walkies.walkie.domain.member.service.dto.request.MemberUpdateCharacterRequestDto;
 import site.walkies.walkie.domain.member.service.dto.request.MemberUpdateRequestDto;
 import site.walkies.walkie.domain.member.service.dto.response.MemberResponseDto;
 import site.walkies.walkie.global.web.exception.CustomException;
@@ -14,6 +17,7 @@ import site.walkies.walkie.global.web.exception.ErrorCode;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final UserCharacterRepository characterRepository;
 
     // 사용자 정보 조회
     @Transactional(readOnly = true)
@@ -29,6 +33,17 @@ public class MemberService {
         if(memberUpdateRequestDto.getMemberNickname() != null){
             member.changeNickname(memberUpdateRequestDto.getMemberNickname());
         }
+        return convertMemberToResponseDto(member);
+    }
+
+    // 함께 걷는 캐릭터 변경
+    @Transactional
+    public MemberResponseDto updateMemberLevelingCharacter(Long memberId, MemberUpdateCharacterRequestDto memberUpdateCharacterRequestDto){
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        UserCharacter character = characterRepository.findById(memberUpdateCharacterRequestDto.getCharacterId()).orElseThrow(() -> new CustomException(ErrorCode.CHARACTER_NOT_FOUND));
+
+        member.changeLevelingUserCharacter(character);
+
         return convertMemberToResponseDto(member);
     }
 
