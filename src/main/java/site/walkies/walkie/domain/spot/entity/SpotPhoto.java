6dcf6d8f8d491cacad799f972f60ea5f78f2762d
김수영 <kswim8914@gmail.com>
2025-03-27 20:57:36 +0900

    feat: SpotPhoto 엔티티 추가

---
+++ src/main/java/site/walkies/walkie/domain/spot/entity/SpotPhoto.java
@@ -0,0 +1,22 @@
+package site.walkies.walkie.domain.spot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "spot_photo")
+@Getter
+public class SpotPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spot_id")
+    private Spot spot;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+}
+
