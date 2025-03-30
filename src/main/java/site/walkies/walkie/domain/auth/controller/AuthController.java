3b770a99074eea96d24cb95b01f40d2f84b231b4
김수영 <kswim8914@gmail.com>
2025-03-30 13:46:59 +0900

    feat: 소셜 로그인 여부 확인하는 함수 추가

--- src/main/java/site/walkies/walkie/domain/auth/controller/AuthController.java
+++ src/main/java/site/walkies/walkie/domain/auth/controller/AuthController.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import site.walkies.walkie.domain.auth.service.AuthService;
-import site.walkies.walkie.domain.auth.service.dto.request.LoginRequestDto;
+import site.walkies.walkie.domain.auth.service.dto.request.AuthCheckRequestDto;
+import site.walkies.walkie.domain.auth.service.dto.request.AuthSignupRequestDto;
 import site.walkies.walkie.domain.auth.service.dto.response.LoginResponseDto;
 import site.walkies.walkie.global.web.dto.response.SuccessResponse;
 
@@ -17,18 +18,19 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Operation(
-            summary = "소셜 로그인 요청",
-            description = "카카오 또는 애플 계정을 통한 소셜 로그인을 처리합니다. " +
-                    "요청 본문에는 provider(kakao 또는 apple)와 각 플랫폼의 access token이 포함됩니다."
-    )
-    @PostMapping("/login")
-    public SuccessResponse<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto) {
-        log.info("[AuthController] Received login request for provider: {}", requestDto.getProvider());
+    @Operation(summary = "소셜 로그인 여부 확인", description = "카카오 또는 애플 accessToken을 기반으로 기존 회원 여부를 확인합니다.")
+    @PostMapping("/check")
+    public SuccessResponse<LoginResponseDto> check(@RequestBody AuthCheckRequestDto requestDto) {
+        log.info("[AuthController] Check login for provider: {}", requestDto.getProvider());
+        LoginResponseDto loginResponseDto = authService.checkUserExists(requestDto);
+        return SuccessResponse.ok(loginResponseDto);
+    }
 
-        // provider에 따라 로그인 처리
-        LoginResponseDto loginResponseDto = authService.login(requestDto);
-
+    @Operation(summary = "소셜 회원가입", description = "카카오 또는 애플 accessToken과 nickname 등을 받아 회원가입을 처리합니다.")
+    @PostMapping("/signup")
+    public SuccessResponse<LoginResponseDto> signup(@RequestBody AuthSignupRequestDto requestDto) {
+        log.info("[AuthController] Signup request for provider: {}", requestDto.getProvider());
+        LoginResponseDto loginResponseDto = authService.signupNewUser(requestDto);
         return SuccessResponse.ok(loginResponseDto);
     }
 }
