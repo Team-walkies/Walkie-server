67a9b58d59ac60fe77585d18c8840bc690b421db
김수영 <kswim8914@gmail.com>
2025-03-30 14:07:12 +0900

    fix: 기존 로그인 로직을 체크와 회원가입 2개로 나눔

--- src/main/java/site/walkies/walkie/domain/member/service/MemberLoginService.java
+++ src/main/java/site/walkies/walkie/domain/member/service/MemberLoginService.java
@@ -15,22 +15,29 @@ public class MemberLoginService {
 
     private final MemberRepository memberRepository;
 
-    // 카카오 로그인
-    public MemberResponseDto findOrCreateKakaoMember(KakaoUserInfoResponseDto userInfo) {
+    // 로그인 확인용 (회원 조회만)
+    public MemberResponseDto findKakaoMember(KakaoUserInfoResponseDto userInfo) {
         String providerId = userInfo.getId().toString();
-        return findOrCreateMember("kakao", providerId, userInfo.getKakaoAccount().getProfile().getNickName());
-    }
-
-    // 애플 로그인
-    public MemberResponseDto findOrCreateAppleMember(String appleUserId) {
-        return findOrCreateMember("apple", appleUserId, "사과 워키");
-    }
-
-    // 기존 멤버인지, 아닌지 확인
-    private MemberResponseDto findOrCreateMember(String provider, String providerId, String nickname) {
         return memberRepository.findByProviderId(providerId)
                 .map(this::convertMemberToMemberResponseDto)
-                .orElseGet(() -> createMember(provider, providerId, nickname));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    public MemberResponseDto findAppleMember(String appleUserId) {
+        return memberRepository.findByProviderId(appleUserId)
+                .map(this::convertMemberToMemberResponseDto)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    // 카카오 회원가입 처리
+    public MemberResponseDto createKakaoMember(KakaoUserInfoResponseDto userInfo, String nickname) {
+        String providerId = userInfo.getId().toString();
+        return createMember("kakao", providerId, nickname);
+    }
+
+    // 애플 회원가입 처리
+    public MemberResponseDto createAppleMember(String appleUserId, String nickname) {
+        return createMember("apple", appleUserId, nickname);
     }
 
     // memberId로 DB에서 정보 가져오기
