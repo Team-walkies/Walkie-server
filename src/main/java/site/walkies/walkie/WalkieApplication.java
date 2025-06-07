0f9844cbdd124335d0b54dfe61436743f7ce69bd
김수영 <kswim8914@gmail.com>
2025-06-08 00:06:52 +0900

    feat: 서버 끊김 현상을 모니터링 하기 위한 서버 재시작 로그 추가

--- src/main/java/site/walkies/walkie/WalkieApplication.java
+++ src/main/java/site/walkies/walkie/WalkieApplication.java
@@ -1,5 +1,6 @@
 package site.walkies.walkie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,12 +8,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication()
+@Slf4j
 public class WalkieApplication {
 
 	public static void main(String[] args) {
 		System.out.println("HealthCheck started");
 		SpringApplication.run(WalkieApplication.class, args);
 		System.out.println("HealthCheck finished");
+		log.info("[Startup] Walkie 서버가 성공적으로 시작되었습니다.");
 	}
 
 }
