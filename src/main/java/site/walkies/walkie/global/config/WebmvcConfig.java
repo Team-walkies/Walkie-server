707f240841d3a0dd1b4478a4632291c0d40810ab
김수영 <kswim8914@gmail.com>
2025-06-08 00:07:10 +0900

    feat: 요청 로그에 대한 인터셉트 설정 추가

--- src/main/java/site/walkies/walkie/global/config/WebmvcConfig.java
+++ src/main/java/site/walkies/walkie/global/config/WebmvcConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import site.walkies.walkie.global.logging.RequestLoggingInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,5 +18,11 @@ public class WebmvcConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3000);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestLoggingInterceptor())
+                .addPathPatterns("/**"); // 모든 요청 로그 수집
     }
 }
