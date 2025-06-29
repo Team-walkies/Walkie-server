4cf52b86ad4d77776a80e1e016e96637eaf2f06b
김수영 <kswim8914@gmail.com>
2025-06-29 13:17:13 +0900

    feat: 이벤트 컨트롤러 클래스 생성, 7월 알 얻기 이벤트에 대한 요청을 받는 메소드 구현

---
+++ src/main/java/site/walkies/walkie/domain/event/controller/EventController.java
@@ -0,0 +1,26 @@
+package site.walkies.walkie.domain.event.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.walkies.walkie.domain.event.service.DailyEggEventService;
+import site.walkies.walkie.domain.event.service.dto.DailyEggEventResponse;
+import site.walkies.walkie.global.auth.dto.MemberPrincipal;
+import site.walkies.walkie.global.web.dto.response.SuccessResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/events")
+public class EventController {
+
+    private final DailyEggEventService dailyEggEventService;
+
+    @GetMapping("/daily-egg")
+    public SuccessResponse<DailyEggEventResponse> getDailyEgg(@AuthenticationPrincipal MemberPrincipal principal) {
+        return SuccessResponse.ok(
+            dailyEggEventService.provideDailyEgg(principal.getMemberId())
+        );
+    }
+}
