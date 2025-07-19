cfa255c6c628f3fe2975518c05a1a5d3f4dac498
jaejunlee11 <sktks11@naver.com>
2025-07-19 12:00:53 +0900

    fix : 헬스케어 current DB 날짜 추가로 인한 서비스 재구축 및 update api 날짜 입력 추가

--- src/main/java/site/walkies/walkie/domain/health/service/dto/request/HealthMoveUpdateRequestDto.java
+++ src/main/java/site/walkies/walkie/domain/health/service/dto/request/HealthMoveUpdateRequestDto.java
@@ -2,10 +2,13 @@ package site.walkies.walkie.domain.health.service.dto.request;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class HealthMoveUpdateRequestDto {
     private Integer targetSteps;
     private Integer nowSteps;
     private Double nowDistance;
     private Double nowCalories;
+    private LocalDate nowDay;
 }
