84a883c5ff3b101c955f8d7e18b1bdd0bd872d9e
jaejunlee11 <sktks11@naver.com>
2025-07-19 12:00:29 +0900

    fix : 헬스케어 current DB 날짜 추가로 인한 repository 변경

--- src/main/java/site/walkies/walkie/domain/health/repository/HealthCurrentRepository.java
+++ src/main/java/site/walkies/walkie/domain/health/repository/HealthCurrentRepository.java
@@ -3,8 +3,10 @@ package site.walkies.walkie.domain.health.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.walkies.walkie.domain.health.entity.HealthCurrent;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface HealthCurrentRepository extends JpaRepository<HealthCurrent, Long> {
-    Optional<HealthCurrent> findByMemberId(Long memberId);
+//    Optional<HealthCurrent> findByMemberId(Long memberId);
+    Optional<HealthCurrent> findByMemberIdAndNowDay(Long memberId, LocalDate nowDay);
 }
