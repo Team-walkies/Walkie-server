9d22be785b6addda6c5569f86411ff963c30f693
Sooyoung <kswim8914@gmail.com>
2025-08-12 21:06:38 +0900

    feat: token 값 유효성 검증 어노테이션 추가

--- src/main/java/site/walkies/walkie/domain/notification/service/dto/request/FcmSendRequestDto.java
+++ src/main/java/site/walkies/walkie/domain/notification/service/dto/request/FcmSendRequestDto.java
@@ -1,9 +1,15 @@
 package site.walkies.walkie.domain.notification.service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FcmSendRequestDto {
+    @NotBlank(message = "FCM 토큰은 필수입니다.")
     private String token;
     private String title;
     private String body;
