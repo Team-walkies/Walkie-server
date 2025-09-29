5cfa7a1c7f62a296a4b2eee42db5574387aa1bbe
jaejunlee11 <sktks11@naver.com>
2025-09-29 23:05:34 +0900

    fix : healthcareEggAcquiredAt로 파라미터 변경

--- src/main/java/site/walkies/walkie/domain/egg/service/dto/request/CreateEggRequest.java
+++ src/main/java/site/walkies/walkie/domain/egg/service/dto/request/CreateEggRequest.java
@@ -9,5 +9,5 @@ public class CreateEggRequest {
     // null 값 처리르 위해서 defualt 값 설정
     private Double latitude = -1d;
     private Double longitude = -1d;
-    private LocalDate getHealthEggDate;
+    private LocalDate healthcareEggAcquiredAt;
 }
